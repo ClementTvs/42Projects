1b76c4fff56befaa298b280f21e0efe726fd9c79
Clement Travers <ctravers@c1r2p10.42perpignan.fr>
2025-06-26 15:06:35 +0200

    docs: add Doxygen documentation for Account class

--- CPP/CPP00/ex02/Account.cpp
+++ CPP/CPP00/ex02/Account.cpp
@@ -1,5 +1,13 @@
 #include "Account.hpp"
 
+/**
+ * @brief Construct a new Account:: Account object
+ * 
+ * Initializes a new account with the given initial deposit.
+ * Increments the total number of accounts and updates the global total amount.
+ * 
+ * @param initial_deposit The amount of the initial deposit for the account
+ */
 Account::Account(int initial_deposit){
 	_accountIndex = _nbAccounts;
 	_amount = initial_deposit;
@@ -15,6 +23,11 @@ Account::Account(int initial_deposit){
 			  << ";created" << std::endl;
 }
 
+/**
+ * @brief Destroy the Account object.
+ * 
+ * Destroy the account displaying the account index, current funds and a closure message.
+ */
 Account::~Account(){
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex
@@ -27,6 +40,11 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+/**
+ * @brief Display the current timestamp.
+ * 
+ * Display the current time in the [YYYYMMDD_HHMMSS] format.
+ */
 void Account::_displayTimestamp(){
 	std::time_t now = std::time(0);
 	std::tm* timeinfo = std::localtime(&now);
@@ -43,6 +61,11 @@ void Account::_displayTimestamp(){
 
 }
 
+/**
+ * @brief Display the informations of all the account.
+ * 
+ * This includes the number of accounts, total amount, number of deposits, number of withdrawals.
+ */
 void Account::displayAccountsInfos(void){
 	_displayTimestamp();
 	std::cout << "accounts:" << _nbAccounts
@@ -52,6 +75,11 @@ void Account::displayAccountsInfos(void){
 			  << std::endl;
 }
 
+/**
+ * @brief Display the current status of the account.
+ * 
+ * This includes the account index, available amount of money, number of deposits and number of withdrawal.
+ */
 void Account::displayStatus(void) const{
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex
@@ -61,6 +89,14 @@ void Account::displayStatus(void) const{
 			  << std::endl; 
 }
 
+/**
+ * @brief Adds a deposit to the account.
+ * 
+ * Update the account's balance and increments both the individual
+ * and global deposit counters
+ *
+ * @param deposit The amount to deposit.
+ */
 void Account::makeDeposit(int deposit){
 	int	old_amount = _amount;
 	_amount += deposit;
@@ -76,6 +112,17 @@ void Account::makeDeposit(int deposit){
 			  << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
+/**
+ * @brief Attemps to withdraw a specified amount from the account
+ * 
+ * If the account has sufficient funds, the withdrawal is processed and
+ * both the individual and global counters are updated.Otherwise, the
+ * withdrawal is refused.
+ *
+ * @param withdrawal The amount to withdraw
+ * @return true If the account had enough funds and the withdrawal succeeded
+ * @return false If the account had insufficient funds and the withdrawal was refused
+ */
 bool Account::makeWithdrawal(int withdrawal){
 	if (_amount >= withdrawal)
 	{
