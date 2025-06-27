a991d8d3cc92d85bc8ff42de9c443fc0ed35a1d6
Clement Travers <ctravers@c1r2p10.42perpignan.fr>
2025-06-27 13:53:26 +0200

    fix: forgot a d on an address print

--- CPP/CPP01/ex02/main.cpp
+++ CPP/CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ctravers <ctravers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 10:44:40 by ctravers          #+#    #+#             */
-/*   Updated: 2025/06/27 11:14:48 by ctravers         ###   ########.fr       */
+/*   Updated: 2025/06/27 13:52:41 by ctravers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main(){
 	std::string *stringPTR = &string;
 	std::string& stringREF = string;
 
-	std::cout << "Adress of the string:           " << &string << std::endl;
-	std::cout << "Adress of the string pointer:   " << stringPTR << std::endl;
-	std::cout << "Adress of the string reference: " << &stringREF << std::endl;
-	std::cout << "The string value:               " << string << std::endl;
-	std::cout << "The stringPTR value:            " << *stringPTR << std::endl;
-	std::cout << "The stringREF value:            " << stringREF << std::endl;
+	std::cout << "Address of the string:           " << &string << std::endl;
+	std::cout << "Address of the string pointer:   " << stringPTR << std::endl;
+	std::cout << "Address of the string reference: " << &stringREF << std::endl;
+	std::cout << "The string value:                " << string << std::endl;
+	std::cout << "The stringPTR value:             " << *stringPTR << std::endl;
+	std::cout << "The stringREF value:             " << stringREF << std::endl;
 }
