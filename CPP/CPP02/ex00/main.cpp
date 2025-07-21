f2d3b1017d8d1229966d4b4f5a66da22d96c8d75
Clement Travers <clementtravers010@gmail.com>
2025-07-21 14:03:31 +0200

    feat: add test

--- CPP/CPP02/ex00/main.cpp
+++ CPP/CPP02/ex00/main.cpp
@@ -11,6 +11,7 @@ int main( void ) {
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-
+	a.setRawBits(89);
+	std::cout << a.getRawBits() << std::endl;
 	return 0;
 }
